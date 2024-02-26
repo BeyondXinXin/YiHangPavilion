29bb1250dcff99863c6100fd18df82e2e139db00
BeyondXinXin <779354187@qq.com>
2024-02-26 17:29:19 +0800

    backup

--- components/aggregated-search.vue
+++ components/aggregated-search.vue
@@ -4,12 +4,7 @@
     <MainClock />
 
     <div class="relative w-[600px] mx-auto mt-15 ">
-      <svg class=" top-0 left-3 w-6 h-6 mt-3 absolute" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-        stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3"
-          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-      </svg>
-
+      <div class="i-mdi-search top-0 left-3 w-6 h-6 mt-3 absolute"></div>
       <input v-model="searchQuery" @keyup.enter="search" :placeholder="currentProviderPlaceholder"
         placeholder="placeholder" class="h-12 w-full px-3 py-2 pl-12 rounded-3 shadow-sm focus:outline-none focus:ring
        placeholder-gray-400  bg-c_bg_input border border-gray-300 
@@ -53,7 +48,9 @@ function search() {
 
 function setProvider(provider) {
   currentProvider.value = provider;
-  // if (searchQuery.value) search();
+  if (searchQuery.value) {
+    search();
+  }
 }
 
 function openSearch(provider) {
