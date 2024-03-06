edb472afe36d6fd6d752cb96f113b3746066f062
BeyondXin <779354187@qq.com>
2024-03-06 17:35:31 +0800

    1. backup

--- components/aggregated-search.vue
+++ components/aggregated-search.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col items-center text-lg ">
+  <div class="flex flex-col items-center text-lg">
 
     <MainClock />
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="w-[1200px]">
     <AggregatedSearch />
-    <div class="flex flex-col gap-12 relative pl-40 pt-10">
-      <div class="flex flex-wrap gap-6">
+    <div class="flex justify-center pt-10">
+      <div class="grid grid-cols-5 gap-4">
         <SiteItemCard v-for="link in bookmarkStore.getGeneral" :key="link.link" :link="link" />
       </div>
     </div>
   </div>
-</template>  
-  
+</template>
+
 <script setup>
 import AggregatedSearch from '~/components/aggregated-search.vue';
 import SiteItemCard from '~/components/site-item-card.vue';
@@ -17,4 +17,4 @@ import { useBookmarkStore } from '~/store/bookmarks';
 const bookmarkStore = useBookmarkStore();
 bookmarkStore.initializeBookmarks();
 
-</script>  
+</script>
