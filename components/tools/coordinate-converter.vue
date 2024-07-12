f8b2a7b67d06a401d9a0a4621fd45dc8a72c754f
BeyondXinXin <779354187@qq.com>
2024-07-12 17:06:05 +0800

    【fix】 无法提交

---
+++ components/tools/coordinate-converter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flex flex-col lg:flex-row max-w-screen-xl mx-auto">
+
+    <div class="flex-1">
+      <In @numberChange="handleNumberChange" />
+    </div>
+
+    <div class="flex-1 lg:ml-10">
+      <Out ref="outRef" />
+    </div>
+
+  </div>
+</template>
+
+
+<script>
+import In from "./coordinate-converter/in.vue";
+import Out from "./coordinate-converter/out.vue";
+
+export default {
+  components: {
+    In,
+    Out,
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+    handleNumberChange(newNumber) {
+      this.$refs.outRef.handleInput(newNumber)
+    },
+  },
+};
+</script>
